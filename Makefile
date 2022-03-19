fe0c82a0433af86bbbb5ee7da8819b02379d0cfb
张炎泼 <drdr.xp@gmail.com>
2022-03-19 14:20:26 +0800

    refactor: update gen-proto.sh for go 1.17

--- Makefile
+++ Makefile
@@ -1,3 +1,2 @@
 gen:
-	# go get github.com/golang/protobuf/{proto,protoc-gen-go}@v1.2.0
-	protoc --proto_path=proto --go_out=plugins=grpc:paxoskv paxoskv.proto
+	./gen-proto.sh
--- gen-proto.sh
+++ gen-proto.sh
@@ -5,9 +5,10 @@
 # go get github.com/gogo/protobuf/protoc-gen-gogofaster
 # go get github.com/gogo/protobuf/protoc-gen-gogoslick
 
+echo "GOPATH: ($GOPATH)"
+
 protoc -I=. \
-    -I=$GOPATH/src \
-    -I=$GOPATH/src/github.com/gogo/protobuf/protobuf \
+    -I=$GOPATH/pkg/mod/github.com/gogo/protobuf@v1.3.2 \
     --proto_path=proto \
     --gogofaster_out=plugins=grpc:paxoskv \
     paxoskv.proto
